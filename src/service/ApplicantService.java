04bfb4dc2979b5297e9b04ffb9c78532aa52951f
emiryucell <e.talhayucel@gmail.com>
2024-11-20 01:13:13 +0000

    Backbone and project structure process finished. There will be adjustments according to future needs.

---
+++ src/service/ApplicantService.java
@@ -0,0 +1,60 @@
+package service;
+
+public class ApplicantService {
+
+
+    public static void submitAssessmentForm(){
+    }
+    public static void viewAssessment(){
+
+    }
+    public static void submitInterviewForm(){
+
+    }
+    public static void viewInterview(){
+
+    }
+    public static void viewFeedback(){
+
+    }
+    public static void viewJobPost(){
+
+    }
+    public static void viewApplicationForm(){
+
+    }
+    public static void viewAllAvailableJobs(){
+
+    }
+    public static void submitApplicationForm(){
+
+    }
+    public static void viewApplicantDashboard(){
+
+    }
+    public static void viewApplicantProfilePage(){
+
+    }
+    public static void deleteApplicantProfile(){
+
+    }
+    public static void updateApplicantProfile(){
+
+    }
+    public static void viewApplicantApplications(){
+
+    }
+    public static void viewSpecificApplication(){
+
+    }
+    public static void withdrawApplication(){
+
+    }
+    public static void viewJobDescFromApplication(){
+
+    }
+    public static void viewApplicationProcessDashboard(){}
+
+
+
+}
---
+++ src/service/CommonService.java
@@ -0,0 +1,35 @@
+package service;
+
+public class CommonService {
+
+    public static void accessLandingPage(){
+
+
+    }
+
+    public static void viewSignInPage(){
+
+    }
+    public static void signIn(){
+
+    }
+    public static void viewSignUpPage(){
+
+    }
+    public static void signUp(){
+
+    }
+    public static void logOut(){
+
+    }
+    public static void viewResetPasswordPage(){
+
+    }
+    public static void resetPassword(){
+
+    }
+
+
+
+
+}
