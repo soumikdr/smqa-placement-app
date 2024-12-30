f2104a66ef5f6ceac5e3117665987c3030f8bb7c
emiryucell <e.talhayucel@gmail.com>
2024-12-30 16:40:13 +0000

    Adjustments for recruiterSignUp. Reverting the sdk.

--- src/service/RecruiterService.java
+++ src/service/RecruiterService.java
@@ -131,7 +131,30 @@ public class RecruiterService {
         }
 
     }
+    // ETY1 - STORY 5
+    public void recruiterSignUp(String recruiterCode, String firstName, String lastName, String userName, String password) {
+        System.out.println("Sign Up processing.... \n");
 
+        RecruiterService recruiterService = new RecruiterService();
+        String id = UUID.randomUUID().toString();
+        if (recruiterCode != null && recruiterCode.equals("XVQTY")) {
+            Recruiter newRecruiter = new Recruiter(id, firstName, lastName, userName, password);
+            Utility.getUsers().add(newRecruiter);
+            System.out.println("Sign Up Successful for Recruiter");
+            Utility.setCurrentUser(newRecruiter);
+            System.out.println("directing to Recruiter Dashboard");
+            viewRecruiterDashboard();
+        }
+        else {
+            System.out.println("Invalid Attempt");
+            System.out.println("Try Again..");
+            recruiterViewSignUp();
+        }
+
+    }
+    public void recruiterViewSignUp(){
+
+    }
 
     public void approveRejectApplication(Application application) {
 
